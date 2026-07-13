8bd23ab5c388cb13e50668f6f620f194c75427ac
fahdi <info@fahdmurtaza.com>
2026-07-13 16:00:31 +0500

    Add progress-aware Continue Day N hero CTA

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import ContinueButton from './ContinueButton.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-actions-after>
+      <ContinueButton />
+    </template>
+  </Layout>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import ProgressTracker from './ProgressTracker.vue'
+import Layout from './Layout.vue'
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component('ProgressTracker', ProgressTracker)
   }
